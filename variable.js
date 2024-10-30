84644a57ca0a99f0a93032775716df2644536a98
pranotosh-sarkar <pranotosh02@gmail.com>
2024-10-30 22:36:39 +0530

    var, let, const & data types

--- variable.js
+++ variable.js
@@ -1,1 +1,20 @@
 console.log("hello world");
+console.log("Today is bhoot chaturdasi");
+let a=5;
+let b='hello';
+let isTrue=false;
+let n=null;
+let u;
+let big=BigInt("500000");
+let s= Symbol("wow!");
+const arr=[1,2,3,4];
+const student={
+    name:"jadu",
+    roll:5,
+    dept:"CSS"
+}
+
+console.log(arr[1]);
+console.log(student["name"],student.roll)
+
+console.log(typeof a, typeof b, typeof isTrue, typeof n,typeof u , typeof big, typeof s, typeof arr,typeof student);
